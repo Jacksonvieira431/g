e8c2b6e42cca171b79605ae8282d4f05ce5a5cdf
Jacksonvieira431 <jacksonhudson431@gmail.com>
2023-07-16 14:29:18 +0000

    Arquivo csv novo, script agora usa o scatterplot e o png scatterplot

--- gasolina.py
+++ gasolina.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 gasolina_df = pd.read_csv('/content/gasolina/gasolina.csv')
-grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda')
-fig = grafico.get_figure()
-fig.savefig('/content/gasolina/gasolina.png')
+grafico = sns.scatterplot(data=gasolina_df, x='dia', y='venda')
+grafico.set_xlabel('Dia')
+grafico.set_ylabel('Venda')
+grafico.set_title('Vendas de Gasolina')
+plt.savefig('/content/gasolina/gasolina.png')
+plt.show()
